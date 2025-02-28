b30486b63a57a3bca24d27d9be115e5283c8018d
abenev <156143875+abenev@users.noreply.github.com>
2025-02-28 09:04:11 -0500

    added chat header

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Financial Accounting Expert`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Custom AI by Arina`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
