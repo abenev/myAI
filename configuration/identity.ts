570e82e7af4fe8061ec0578721e9e885bd0e9d64
abenev <156143875+abenev@users.noreply.github.com>
2025-03-06 14:44:20 -0500

    customized OWNER_DESCRIPTION in identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Arina`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Arina is a determined and financially-responsible BUSI 488 student!`;
 
 export const AI_NAME: string = `FinWise`;
 export const AI_TONE: string = `[AI TONE]`;
