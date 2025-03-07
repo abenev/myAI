7811d2bc210d069ee3c367d66517658ce6db1340
abenev <156143875+abenev@users.noreply.github.com>
2025-03-06 21:12:37 -0500

    customized AI_TONE in identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Arina`;
 export const OWNER_DESCRIPTION: string = `Arina is a determined and financially-responsible BUSI 488 student!`;
 
 export const AI_NAME: string = `FinWise`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Peppy and cute`;
 export const AI_ROLE: string = `[AI ROLE]`;
