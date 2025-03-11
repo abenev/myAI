ba98e9a6c565f69abf50d8254d9c7f65c2ea770a
abenev <156143875+abenev@users.noreply.github.com>
2025-03-10 20:17:01 -0500

    customized ai role in identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `Arina is a determined and financially-
 
 export const AI_NAME: string = `FinWise`;
 export const AI_TONE: string = `Cheerful and cute`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_ROLE: string = `A helpful expert in all things financial accounting, here to simplify financial topics while spreading a positive attitude`;
